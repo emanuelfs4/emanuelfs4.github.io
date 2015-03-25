393b7c45bd02ec7ce46aaecc0d2e1e9d6222f476
Emanuel Felipe <emanuelfs4@gmail.com>
2015-03-25 23:58:28 +0000

    Initial Homepage.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var myName = "about";
+
+var letterColors =  [white];
+
+if(myName.length > 3) {
+    bubbleShape = "circle";
+}
+
+else {
+    bubbleShape = "square";
+}
+
+
+drawName(myName, letterColors);
+bounceBubbles();
